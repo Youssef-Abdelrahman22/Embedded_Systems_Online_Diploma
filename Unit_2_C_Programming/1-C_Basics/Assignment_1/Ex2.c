ca69a3af72a1e8b8636d9fb415f781e2f4ada526
Youssef Abdelrahman <youssefabdelrahman893@gmail.com>
2023-10-21 16:52:33 +0300

    Print a Integer Entered by a User

---
+++ Unit_2_C_Programming/1-C_Basics/Assignment_1/Ex2.c
@@ -0,0 +1,18 @@
+/*
+ * Ex2.c
+ *
+ *  Created on: Aug 1, 2023
+ *      Author: Youssef
+ */
+#include"stdio.h"
+void main()
+{
+	printf("##########Console-output###\n");
+	printf("Enter an Integer: ");
+	int Number=0;
+	fflush(stdout);
+	scanf("%d",&Number);
+	printf("You Entered : %d\n",Number);
+	printf("###########################");
+	printf("\n\r########################################################################");
+}
