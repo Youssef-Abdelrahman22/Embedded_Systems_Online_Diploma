dd49195159bd49d027ff48cce8f78ba5a83e5c8c
Youssef Abdelrahman <youssefabdelrahman893@gmail.com>
2023-10-21 16:53:06 +0300

    Add Two Integers

---
+++ Unit_2_C_Programming/1-C_Basics/Assignment_1/EX3.c
@@ -0,0 +1,20 @@
+/*
+ * EX3.c
+ *
+ *  Created on: Aug 10, 2023
+ *      Author: Youssef
+ */
+#include "stdio.h"
+void main()
+{
+	int A ,B;
+	printf("##########Console-output###\rEnter Two integers:");
+	fflush(stdout);
+	fflush(stdin);
+	scanf("%d",&A);
+	scanf("%d",&B);
+	printf("Sum:%d\n",A+B);
+	printf("###########################\n\r");
+	printf("########################################################################");
+}
+
