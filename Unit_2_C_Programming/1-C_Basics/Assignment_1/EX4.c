0cdb9f3553ca1a17653065842b5a403e1f7ed7ec
Youssef Abdelrahman <youssefabdelrahman893@gmail.com>
2023-10-21 16:53:47 +0300

    Multiply two Floating Point Numbers

---
+++ Unit_2_C_Programming/1-C_Basics/Assignment_1/EX4.c
@@ -0,0 +1,18 @@
+/*
+ * EX4.c
+ *
+ *  Created on: Aug 10, 2023
+ *      Author: Youssef
+ */
+#include "stdio.h"
+void main()
+{
+	float A , B;
+	printf("##########Console-output###\n\r###########################\n");
+	printf("Enter Two Numbers:");
+	fflush(stdin);fflush(stdout);
+	scanf("%f%f",&A,&B);
+	printf("Product:%f\n",A*B);
+	printf("#################################################################");
+}
+
