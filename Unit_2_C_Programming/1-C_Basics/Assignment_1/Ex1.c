8b1b82f8a526b102e0e1f39d754a6aa6735358e1
Youssef Abdelrahman <youssefabdelrahman893@gmail.com>
2023-10-21 16:51:56 +0300

    Print a Sentence

---
+++ Unit_2_C_Programming/1-C_Basics/Assignment_1/Ex1.c
@@ -0,0 +1,12 @@
+/*
+ * Ex1.c
+ *
+ *  Created on: Aug 1, 2023
+ *      Author: Youssef
+ */
+#include "stdio.h"
+void main()
+{
+	printf("##########Console-output###\nC Programming\n###########################");
+	printf("\n\r########################################################################");
+}
